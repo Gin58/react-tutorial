53cbf3719a2c3c81d649520cb0debf88ee2b59f6
Gin58 <invincible.gnj1875@gmail.com>
2019-12-17 23:01:52 +0900

    button

--- react_router/src/js/client.js
+++ react_router/src/js/client.js
@@ -9,13 +9,13 @@ import Settings from "./pages/Settings";
 
 const app = document.getElementById('app');
 
-ReactDOM.render(<Layout />, app);
 ReactDOM.render(
   <Router>
     <Layout>
       <Route exact path="/" component={Featured}></Route>
-      <Route path="/archives" component={Archives}></Route>
-      <Route path="/settings" component={Settings}></Route>
+      <Route exact path="/archives" component={Archives}></Route>
+      <Route path="/archives/:article" component={Archives}></Route>
+      <Route path="/settings/:mode(main|extra)" component={Settings}></Route>
     </Layout>
   </Router>,
 app);
