09b848003abf14b86f7fecd1e834f06a36003a96
Gin58 <invincible.gnj1875@gmail.com>
2019-12-23 15:00:59 +0900

    tweet

---
+++ react-redux-minimum/src/js/client.js
@@ -0,0 +1,38 @@
+import { createStore, combineReducers } from "redux";
+
+const userReducer = (state = {}, action) => {
+  switch(action.type) {
+    case "CHANGE_NAME":
+      state = {...state, name: action.payload}
+      break;
+    case "CHANGE_AGE":
+      state = {...state, age: action.payload}
+      break;
+  }
+  return state;
+}
+
+const tweetsReducer = (state = [], action) => {
+  switch(action.type) {
+    case "ADD_TWEET":
+      state = state.concat({id: Date.now(), text: action.payload});
+  }
+  return state;
+}
+
+const reducers = combineReducers({
+  user: userReducer,
+  tweets: tweetsReducer
+});
+
+const store = createStore(reducers);
+
+store.subscribe(() => {
+  console.log("store changed", store.getState());
+});
+
+store.dispatch({type: "CHANGE_NAME", payload: "Tsutomu"});
+store.dispatch({type: "CHANGE_AGE", payload: 35});
+store.dispatch({type: "CHANGE_AGE", payload: 36});
+store.dispatch({type: "ADD_TWEET", payload: "OMG LIKE LOL"});
+store.dispatch({type: "ADD_TWEET", payload: "I am so like seriously like totally like right now"});
